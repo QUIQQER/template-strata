0d3deffb92792993850a360f2e4ba8cb8eb10182
Michael <michael.danielczok@gmail.com>
2015-08-17 16:34:48 +0200

    Scrolleffekt verbessert - jetzt soll die Höche des Contentbereichs in die Berechnung einbezogen werden. CSS Anpassun. Zweiter Footer für Mobilegeräte (kein Scrolleffekt).

--- bin/js/Parallax.js
+++ bin/js/Parallax.js
@@ -42,16 +42,28 @@ define('template-strata/Parallax', [
         load : function()
         {
             this.$Header = document.id('header');
-            //this.$Main   = document.id('main');
+            this.$Main   = document.id('main');
             this.$Wrapper = document.id('header_wrapper');
 
             this.$FX = moofx(this.$Wrapper);
 
             this.$recalc();
 
-            window.addEvent('scroll', QUIFunctionUtils.debounce(this.$onScroll, 20));
+
+            window.addEvent('scroll', QUIFunctionUtils.debounce(this.$onScroll, 17));
             window.addEvent('resize', QUIFunctionUtils.debounce(this.$recalc, 10));
+
+
+            console.log('Contenthöhe (Main): ' + this.$Main.getSize().y + 'px');
+            console.log('Nicht sichtbarer Teil des Contents (Main) ' + (this.$Main.getSize().y - window.getSize().y) +'px');
+
+            console.log('Footerhöhe (Wrapper): ' + this.$Wrapper.getSize().y + 'px');
+            console.log('Nicht sichtbarer Teil des Footers (Wrapper) ' + (this.$Wrapper.getSize().y - window.getSize().y) +'px');
+
+            console.log('Fensterhöhe (Window): ' + window.getSize().y + 'px');
         },
+
+
 
         /**
          * recalc of the main size data
@@ -59,10 +71,13 @@ define('template-strata/Parallax', [
         $recalc : function()
         {
             this.$pos = window.getScroll().y;
-            this.$winSize = window.getSize();
+            this.$MainSize = this.$Main.getSize().y;
+            this.$winSize = window.getSize().y;
             this.$wrapperSize = this.$Wrapper.getSize();
-        },
 
+
+
+        },
         /**
          * on scroll event
          */
@@ -70,15 +85,31 @@ define('template-strata/Parallax', [
         {
             var winPos = window.getScroll().y,
                 diff   = winPos - this.$pos;
+            console.log('-----------------------')
 
 
-            var percent = QUIUtilsMath.percent(diff, this.$winSize.y);
+            console.log('Differenz in px ' + diff);
+
+            console.log('Fenster position ' + winPos );
+
+
+            this.$FooterPos = this.$Wrapper.getStyle('top');
+            console.log('Footer position ' + this.$FooterPos);
+
+            //var percent = QUIUtilsMath.percent(diff, this.$winSize);
+            //console.log(percent + '%');
+            // Neue Berechnung
+            //console.log('um wieviel px soll Footer verschoben werden: ' + ((this.$Wrapper.getSize().y * diff) / this.$Main.getSize().y));
+            console.log('um wieviel px soll Footer verschoben werden (NEU): ' + ((this.$Wrapper.getSize().y * diff) / (this.$MainSize - window.getSize().y)));
 
             this.$pos = winPos;
 
-            if (this.$wrapperSize.y > this.$winSize.y){
+            if (this.$wrapperSize.y > this.$winSize){
 
-                var scrollTo = (percent / 100) * this.$wrapperSize.y * (-1);
+                //var scrollTo = (percent / 100) * this.$wrapperSize.y * (-1);
+
+                // Neue Berechnung
+                scrollTo = (((this.$Wrapper.getSize().y - window.getSize().y) * diff) / (this.$MainSize - window.getSize().y) * (-1));
 
                 this.$FX.animate({
                     top: this.$Wrapper.getStyle('top').toInt() + scrollTo
